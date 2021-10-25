a26dce043819a9e491b3e1c4595116e10ce6564d
zhangyin <zhangyin@newlink.com>
2021-10-25 14:42:06 +0800

    testng1

--- src/test/java/com/newcrud/testngTest/TestTwo.java
+++ src/test/java/com/newcrud/testngTest/TestTwo.java
@@ -7,12 +7,4 @@ public class TestTwo {
     public void testOne(){
         System.out.println("TestTwo的testOne");
     }
-    @BeforeTest
-    public void testThirteen(){
-        System.out.println("@BeforeTest - 对于套件测试，在运行属于<test>标签内的类的任何测试方法之前运行。---TestTwo");
-    }
-    @AfterTest
-    public void testFourteen(){
-        System.out.println("@AfterTest - 对于套件测试，在运行属于<test>标签内的类的所有测试方法都已运行之后运行。------TestTwo");
-    }
 }
